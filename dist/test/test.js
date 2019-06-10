98ae4535837133e8d09839a5f3b88331ce900cbc
Hasan Karahan <hasan.karahan@blackhan.com>
2019-06-10 18:45:21 +0300

    yo @dizmo/module --upgrade

--- dist/test/test.js
+++ dist/test/test.js
@@ -1,18 +1,23 @@
 "use strict";
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var chai_1 = require("chai");
+
 require("mocha");
+
 describe("Dizmo", function () {
-    it("should not exist", function () {
-        chai_1.expect(typeof dizmo === "undefined" ? "undefined" : _typeof(dizmo)).to.eq("undefined");
-    });
-    it("should not be referenceable", function () {
-        chai_1.expect(function () {
-            return dizmo;
-        }).to.throw(ReferenceError);
-    });
+  it("should not exist", function () {
+    chai_1.expect(typeof dizmo === "undefined" ? "undefined" : _typeof(dizmo)).to.eq("undefined");
+  });
+  it("should not be referenceable", function () {
+    chai_1.expect(function () {
+      return dizmo;
+    }).to["throw"](ReferenceError);
+  });
 });
 //# sourceMappingURL=test.js.map
